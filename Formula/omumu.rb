e575ad032195b1ca3f941d1fcaaaba11fedca94b
steni <stenmorten@gmail.com>
2026-03-07 13:56:25 +0100

    Bump to v0.2.1

--- Formula/omumu.rb
+++ Formula/omumu.rb
@@ -1,13 +1,13 @@
 class Omumu < Formula
   desc "CLI for the Omumu customer education platform"
   homepage "https://github.com/omumuas/omumu-cli"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/omumuas/omumu-cli/releases/download/v0.2.0/omumu-0.2.0-darwin-arm64.tar.gz"
-      sha256 "06960c426d0091732295c498c2ddb0f667a457174f47d1efd451cd5040ad1b7e"
+      url "https://github.com/omumuas/omumu-cli/releases/download/v0.2.1/omumu-0.2.1-darwin-arm64.tar.gz"
+      sha256 "4f08ebdac3fc4bcdf69b5ddb0f2e5a0aa3a23149816e0b0279e958ff4ee79943"
     end
   end
 
@@ -16,6 +16,6 @@ class Omumu < Formula
   end
 
   test do
-    assert_match "omumu 0.2.0", shell_output("#{bin}/omumu --version")
+    assert_match "omumu 0.2.1", shell_output("#{bin}/omumu --version")
   end
 end
